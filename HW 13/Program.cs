fc75b00cf79bcbd9685deb131f2c4f91d7a2b632
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-02-03 18:36:32 +0300

    13

---
+++ HW 13/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+int a;
+Console.WriteLine($"Введите целое число: ");
+int.TryParse(Console.ReadLine(), out a);
+
+if (a > 99)
+{
+    string a1 = a.ToString();
+    int b = int.Parse(a1[2].ToString());
+    Console.WriteLine($"Третья цифра: {b}");
+}
+else
+    Console.WriteLine($"Третьей цифры нет");
